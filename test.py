8e211cfe631af06ebb4616babc7b504ead254d50
Xinyu Li <xinyuli@Xinyus-MacBook-Air.local>
2024-09-15 19:08:25 -0700

    initial commit

---
+++ test.py
@@ -0,0 +1,196 @@
+import tkinter as tk
+from tkinter import ttk, scrolledtext
+from ttkbootstrap.constants import *
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import *
+import paramiko
+import threading
+from utils import containers, options
+
+
+# docker start ros-dev-macos && docker exec ros-dev-macos bash -c "source ros_entrypoint.sh && ./setup.sh"
+
+def fetch_and_display_logs(ssh_info, text_widget, container_name):
+    hostname = ssh_info["hostname"]
+    username = ssh_info["username"]
+    password = ssh_info["password"]
+
+    # Create SSH client
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname, username=username, password=password, timeout=10)
+
+    # Command to fetch logs
+    if dropbox.get() == "test":
+        command = f"source .zshrc && docker logs -f {container_name} --follow --since $(date +%s)"
+    else:
+        command = f"docker logs -f {container_name} --follow --since $(date +%s)"
+    stdin, stdout, stderr = ssh.exec_command(command, get_pty=True)
+
+    # Display logs in the Tkinter text widget
+    for line in iter(lambda: stdout.readline(2048), ""):
+        text_widget.insert(tk.END, line)
+        text_widget.yview(tk.END)
+    ssh.close()
+
+
+# def start_logs_streaming(containers, text_widgets):
+#     for i in range(len(containers)):
+#         start_log_streaming(text_widgets[i], containers[i])
+#
+#
+# def start_log_streaming(text_widget, container_name):
+#     selected_option = dropbox.get()
+#     if selected_option in options:
+#         ssh_info = options[selected_option]
+#         threading.Thread(target=fetch_and_display_logs, args=(ssh_info, text_widget, container_name),
+#                          daemon=True).start()
+
+
+# establish ssh
+
+def run_ssh_command(ssh_info, text_widget):
+    hostname = ssh_info["hostname"]
+    username = ssh_info["username"]
+    password = ssh_info["password"]
+    command = ssh_info["command"]
+
+    # create ssh
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        # connect to ssh
+        ssh.connect(hostname, username=username, password=password, timeout=10)
+        text_widget.insert(tk.END, f"successfully connect {hostname}\n")
+
+        # exec
+        stdin, stdout, stderr = ssh.exec_command(command, get_pty=True)
+        while True:
+            line = stdout.readline()
+            if not line:
+                break
+            text_widget.insert(tk.END, line)
+        text_widget.insert(tk.END, "finish\n")
+
+        # output error
+        error = stderr.read().decode()
+        if error:
+            text_widget.insert(tk.END, f"error:\n{error}\n")
+
+    except Exception as e:
+        text_widget.insert(tk.END, f"connection failed: {e}\n")
+    finally:
+        ssh.close()
+
+
+def on_select(text_widget):
+    selected_option = dropbox.get()
+    if selected_option in options:
+        ssh_info = options[selected_option]
+        threading.Thread(target=run_ssh_command, args=(ssh_info, text_widget), daemon=True).start()
+
+
+# Function to clear text widgets
+def clear_text_widgets(text_widgets):
+    for widget in text_widgets:
+        widget.delete('1.0', tk.END)
+
+
+# Function to stop Docker containers
+def stop_docker(ssh_info, container_names):
+    print("[INFO]: start stopping ")
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(ssh_info["hostname"], username=ssh_info["username"], password=ssh_info["password"], timeout=10)
+        for container in container_names:
+            if dropbox.get() == "test":
+                command = f"source .zshrc && docker stop $(docker ps)"
+            else:
+                command = f"docker stop {container}"
+            ssh.exec_command(command)
+    except Exception as e:
+        print(e)
+    finally:
+        for container in container_names:
+            print(f"[INFO]: successfully stopped {container}")
+        ssh.close()
+
+
+# Function to stop Docker containers
+
+def quitt():
+    selected_option = dropbox.get()
+    ssh_info = options[selected_option]
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(ssh_info["hostname"], username=ssh_info["username"], password=ssh_info["password"], timeout=10)
+        if dropbox.get() == "test":
+            command = f"source .zshrc && docker ps -q | xargs --no-run-if-empty docker stop"
+        else:
+            command = f"docker stop docker ps -q | xargs --no-run-if-empty docker stop"
+        ssh.exec_command(command)
+    except Exception as e:
+        print(e)
+    finally:
+        ssh.close()
+
+
+def quit():
+    selected_option = dropbox.get()
+    ssh_info = options[selected_option]
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(ssh_info["hostname"], username=ssh_info["username"], password=ssh_info["password"], timeout=10)
+        if dropbox.get() == "test":
+            command = f"source .zshrc && docker ps -q | xargs --no-run-if-empty docker stop"
+        else:
+            command = f"docker stop docker ps -q | xargs --no-run-if-empty docker stop"
+        ssh.exec_command(command)
+    except Exception as e:
+        print(e)
+    finally:
+        ssh.close()
+        root.quit()
+        print("[INFO]: Quit ")
+
+
+# main
+root = tk.Tk()
+root.title("Orin GUI")
+root.protocol("WM_DELETE_WINDOW", quit)
+
+setting_frame = tk.Frame(root)
+
+label = ttk.Label(setting_frame, text="Select Application：")
+label.grid(row=0, column=0, pady=10, sticky="e" )
+
+# drop box
+dropbox = ttk.Combobox(setting_frame, values=list(options.keys()), width=20,  bootstyle="info-reversed")
+dropbox.current(0)
+dropbox.grid(row=0, column=1, pady=10, sticky="we")
+
+# start button
+start_button = ttk.Button(setting_frame, text="Start", command=lambda: on_select(output_log_text), bootstyle=INFO)
+start_button.grid(row=0, column=2, padx=10,pady=10, sticky="we")
+
+# Button to stop Docker containers
+stop_button = ttk.Button(setting_frame, text="Stop", bootstyle=DANGER, command=lambda: stop_docker(options[dropbox.get()], containers[dropbox.get()]))
+stop_button.grid(row=0, column=3,  padx=10, pady=10, sticky="we")
+
+# Button to clear output
+clear_button = ttk.Button(setting_frame, text="Clear Log", command=lambda: clear_text_widgets([output_log_text]))
+clear_button.grid(row=0, column=4,  padx=10, pady=10, sticky="we")
+
+setting_frame.pack()
+log_frame = tk.Frame(root)
+# output log
+output_log_text = scrolledtext.ScrolledText(log_frame, width=80, height=20)
+output_log_text.grid(row=0, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
+log_frame.pack()
+
+root.mainloop()
---
+++ utils.py
@@ -0,0 +1,102 @@
+
+containers = {
+    "test": [
+        "ros-dev-macos"
+    ],
+
+    "base":
+        ["Work"],
+
+    "fast_lio":
+        ["Work"],
+
+    "camera-detection":
+        ["Work"],
+
+    "perception-reconstruction":
+        ["Work"],
+
+    "dnn":
+        ["Work"],
+
+
+
+}
+
+
+HOSTNAME = "192.168.61.44"
+USERNAME = "junchi"
+PASSWORD = "helloworld"
+
+options = {
+    "base": {
+        "hostname": HOSTNAME,
+        "username": USERNAME,
+        "password": PASSWORD,
+        "command": r"""
+         docker start  Work
+         docker exec Work -c "source install/setup.bash && ros2 launch isaac_ros_argus_camera isaac_ros_argus_camera_stereo.launch.py" &
+         docker exec Work -c "source install/setup.bash && ros2 launch livox_ros_driver2 msg_MID360_launch.py"      
+"""
+    },
+
+    "fast_lio": {
+        "hostname": HOSTNAME,
+        "username": USERNAME,
+        "password": PASSWORD,
+        "command": r"""
+         docker start  Work
+         docker exec Work -c "source install/setup.bash && ros2 launch livox_ros_driver2 msg_custom_MID360_launch.py" &
+         docker exec Work -c "source install/setup.bash && ros2 launch fast_lio launch/mapping.launch.py" 
+"""
+    },
+
+    "dnn": {
+        "hostname": HOSTNAME,
+        "username": USERNAME,
+        "password": PASSWORD,
+        "command": r"""
+             docker start  Work
+             docker exec Work -c "source install/setup.bash && ros2 launch isaac_ros_examples isaac_ros_examples.launch.py launch_fragments:=argus_stereo,rectify_stereo,ess_disparity \
+engine_file_path:=${ISAAC_ROS_WS}/isaac_ros_assets/models/dnn_stereo_disparity/dnn_stereo_disparity_v4.0.0/ess.engine \ threshold:=0.35" 
+    """
+    },
+
+    "camera-detection": {
+        "hostname": HOSTNAME,
+        "username": USERNAME,
+        "password": PASSWORD,
+        "command": r"""
+             docker start  Work
+             docker exec Work -c "source install/setup.bash && ros2 launch isaac_ros_examples isaac_ros_examples.launch.py launch_fragments:=argus_stereo,rectify_stereo,ess_disparity \
+engine_file_path:=${ISAAC_ROS_WS}/isaac_ros_assets/models/dnn_stereo_disparity/dnn_stereo_disparity_v4.0.0/ess.engine \ threshold:=0.35" 
+    """
+    },
+
+    "perception-reconstruction": {
+        "hostname": HOSTNAME,
+        "username": USERNAME,
+        "password": PASSWORD,
+        "command": r"""
+             docker start  Work
+             docker exec Work -c "source install/setup.bash && ros2 launch isaac_ros_examples isaac_ros_examples.launch.py launch_fragments:=argus_stereo,rectify_stereo,ess_disparity \
+engine_file_path:=${ISAAC_ROS_WS}/isaac_ros_assets/models/dnn_stereo_disparity/dnn_stereo_disparity_v4.0.0/ess.engine \ threshold:=0.35" 
+    """
+    },
+
+
+    "test": {
+        "hostname": "localhost",
+        "username": "xinyuli",
+        "password": "1998!1204",
+        "command": r"""
+         source .zshrc   
+         docker start ros-dev-macos
+         echo "start talker" && docker exec ros-dev-macos bash -c "source ros_entrypoint.sh && ros2 run demo_nodes_cpp talker" &
+         echo "start listener" && docker exec ros-dev-macos bash -c "source ros_entrypoint.sh && ros2 run demo_nodes_cpp listener"  
+        """
+
+    },
+}
+
+
